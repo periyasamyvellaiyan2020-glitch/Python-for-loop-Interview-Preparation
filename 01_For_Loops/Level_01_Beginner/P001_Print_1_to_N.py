49f851d990405369274a9912751e9c2d65392494
VP <periyasamyvellaiyan2020@gmail.com>
2026-08-06 12:47:31 +0530

    Add P001 Print 1 to N program

---
+++ 01_For_Loops/Level_01_Beginner/P001_Print_1_to_N.py
@@ -0,0 +1,6 @@
+ #Sequence Print 1 to N
+#Given a positive integer N, write a program using a for loop to print all numbers from 1 to N inclusive, each on a new line.
+
+n=int(input("Enter n:"))
+for i in range(1,n+1):
+    print(i)
