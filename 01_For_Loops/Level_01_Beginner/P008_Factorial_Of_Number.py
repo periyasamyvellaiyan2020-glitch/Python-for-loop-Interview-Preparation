e9b39bf23f36f4054395c83ba1c991d084268972
VP <periyasamyvellaiyan2020@gmail.com>
2026-08-06 16:33:15 +0530

    logic changed

--- 01_For_Loops/Level_01_Beginner/P008_Factorial_Of_Number.py
+++ 01_For_Loops/Level_01_Beginner/P008_Factorial_Of_Number.py
@@ -1,8 +1,6 @@
 #Given a non-negative integer N, compute its factorial (N!) using a for loop. Return 1 if N = 0.
 n=int(input("Enter n:"))
 fact=1
-if n==0:
-    n=1
 for i in range(1,n+1):
     fact=fact*i
 print(fact)
