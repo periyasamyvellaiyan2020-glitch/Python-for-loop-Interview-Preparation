2a93e3e40bdb6564621a607168c0eab425c4ad9e
VP <periyasamyvellaiyan2020@gmail.com>
2026-08-06 10:31:56 +0530

    Add P001 Print 1 to N using for loop

---
+++ 01_For_Loops/Level_01_Beginner/P001_Print_1_to_N.py
@@ -0,0 +1,18 @@
+"""
+Problem : P001
+
+Title : Print numbers from 1 to N
+
+Difficulty : Easy
+
+Interview Frequency : ★★★★★
+
+Pattern : Printing Pattern
+
+Author : VP
+"""
+
+n = int(input("Enter n: "))
+
+for i in range(1, n + 1):
+    print(i)
