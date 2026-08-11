b575caeae44f82c408a34f7fada20f168f938d1f
VP <periyasamyvellaiyan2020@gmail.com>
2026-08-11 12:40:56 +0530

    02_set_count_elements solved

---
+++ 01_For_Loops/Sets/02_set_count_elements.py
@@ -0,0 +1,8 @@
+#Find the number of elements in the set
+numbers = {10, 20, 30, 40, 50,}
+count=0
+for i in numbers:
+    count+=1
+print(f"number of elements in the set {count}")
+
+#print(len(numbers))
