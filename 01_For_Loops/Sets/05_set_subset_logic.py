d03a6f5edf584a5d1d8ad47f91c7a21075fabf7e
VP <periyasamyvellaiyan2020@gmail.com>
2026-08-11 14:01:17 +0530

    05_set_subset_logic solved

---
+++ 01_For_Loops/Sets/05_set_subset_logic.py
@@ -0,0 +1,15 @@
+#build your own for + if logic to determine whether every element of a exists in b.
+a = {10, 20}
+b = {10, 20, 30, 40}
+size=len(a)
+count=0
+for a_element in a:
+    for b_element in b:
+        if a_element==b_element:
+            count+=1
+            break add.
+            
+if size==count:
+    print('a is subset of b')
+else:
+    print("a is not subset of b")
